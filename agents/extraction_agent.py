6d0f2460dd388a84997a5d1031756848029d02c8
BhavikaBaddur <baddurbhavika@gmail.com>
2026-07-01 14:10:29 +0400

    chore: force redeployment to pick up token limit fix

--- agents/extraction_agent.py
+++ agents/extraction_agent.py
@@ -251,4 +251,5 @@ Risk Summary:
         "next_agent": "analysis_agent",
         "extracted_data": full_extracted,
         "messages": state["messages"] + [response]
-    }
+    }#   r e d e p l o y m e n t   t r i g g e r  
+ 
